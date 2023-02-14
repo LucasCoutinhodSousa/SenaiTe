91167d1be0ba8ace6f968d7e090463cb4b957c4e
LucasCoutinhodSousa <124942844+LucasCoutinhodSousa@users.noreply.github.com>
2023-02-13 21:38:50 -0300

    Add files via upload

---
+++ Janela_Mesagem.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package janela;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author aluno
+ */
+public class Janela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i;
+        JFrame janela = new JFrame("Titulo da janela");
+        janela.setBounds(50, 400, 400, 120); //posição e tamanho
+        janela.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        
+        FlowLayout flow = new FlowLayout();
+        
+        Container caixa = janela.getContentPane();
+        caixa.setLayout(flow);
+        
+        for(i=1; i <= 1000; i++)
+        caixa.add(new JButton("Aperte " + i));
+        JOptionPane.showMessageDialog(janela,"MEnsage", "Titu", JOptionPane.WARNING_MESSAGE);
+        janela.setVisible(true);
+       
+    }
+}
