459c174ce5033c875e0177e0398a436ddf540bf2
LucasCoutinhodSousa <124942844+LucasCoutinhodSousa@users.noreply.github.com>
2023-02-16 19:18:07 -0300

    Add files via upload

--- Formulario.java
+++ Formulario.java
@@ -11,7 +11,9 @@ import java.awt.FlowLayout;
 import static java.awt.FlowLayout.LEFT;
 import static java.awt.FlowLayout.RIGHT;
 import java.awt.GridBagLayout;
+import javax.swing.ComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -33,6 +35,8 @@ public class Formulario {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        String anoEscolar[] = {"1 ano", "2 ano", "3 ano"};
+        
         JFrame janela = new JFrame("Formulario");
         janela.setBounds(100, 400, 300, 400);
         janela.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -47,12 +51,12 @@ public class Formulario {
         JLabel nomeProfessor = new JLabel();
         nomeProfessor.setText("NOME");
         
-        JTextField nome = new JTextField(20);
+        JTextField nome = new JTextField(21);
            
         JLabel cpf = new JLabel();
         cpf.setText("CPF");
         
-        JTextField caixaParaCPF = new JTextField(20);
+        JTextField caixaParaCPF = new JTextField(22);
         
         JLabel pass = new JLabel();
         pass.setText("SENHA");
@@ -64,9 +68,13 @@ public class Formulario {
         
         JLabel sexo = new JLabel("Sexo");
         
+        JLabel escolhaDoAnoEscolar = new JLabel("Escolha o ano escolar ");
+        JComboBox escolaDoAno = new JComboBox(anoEscolar);
+        
         
         janela.add(nomeProfessor);
         janela.add(nome);
+        
         
         janela.add(cpf);
         janela.add(caixaParaCPF);
@@ -77,6 +85,9 @@ public class Formulario {
         janela.add(sexo);
         janela.add(masc);
         janela.add(fem);
+        
+        janela.add(escolhaDoAnoEscolar);
+        janela.add(escolaDoAno);
         
         janela.add(new JButton("Entrar"));
         
